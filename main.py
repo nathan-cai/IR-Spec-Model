c06cd3d326d8aaac0cfae47333693d944800436b
Qien Nathan Cai <qien.nathan@gmail.com>
2023-05-21 13:09:14 -0700

    Complete data scraping program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import jcamp
 import csv
+import requests
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -10,17 +11,61 @@ def row_to_write(name, val):
     return result
 
 
+def valid_spec(jcamp_content):
+    if len(jcamp_content["x"]) == 0:
+        return False
+    elif jcamp_content["x"][0] % 1 != 0.0:
+        return False
+    elif 550.0 not in jcamp_content["x"] or 3846.0 not in jcamp_content["x"]:
+        return False
+    else:
+        return True
+
+
+def get_indices(arr):
+    start = -1
+    end = -1
+    for index in range(len(arr)):
+        if arr[index] == 550.0:
+            start = index
+        if arr[index] == 3846.0:
+            end = index
+    return start, end
+
+
 if __name__ == "__main__":
-    d = jcamp.jcamp_readfile("JCampFiles/592-43-8-IR.jdx")
-    print(len(d["x"]))
-    print(len(d["y"]))
-    print(d["x"])
-    print(d["y"])
-    FILE = open('data.csv', 'a')
 
-    writer = csv.writer(FILE)
-    row = row_to_write('2-hexene', d["y"])
+    # Data will be written into data.csv
+    with open('data.csv', 'a', newline='') as FILE:
+        writer = csv.writer(FILE)
 
-    writer.writerow(row)
+        # cas_num.txt includes cas numbers to be used to generate urls to IR Specs
+        cas = open('cas_num.txt', 'r')
 
-    FILE.close()
+        for i in range(44388):
+            # Wrap in try and except due to occurence of uni code errors
+            try:
+                # reads a new entry for cas line
+                curr = cas.readline().strip()
+                currSplit = curr.split(",,")
+                num = currSplit[2].replace('-', '')
+
+                # generate url for cas number
+                url = f'https://webbook.nist.gov/cgi/cbook.cgi?JCAMP=C{num}&Index=0&Type=IR'
+                response = requests.get(url)
+                content = response.content.splitlines()
+                content = [line.decode("utf-8") for line in content]
+                d = jcamp.jcamp_read(content)
+
+                # write to data.csv if the spec is valid
+                if valid_spec(d):
+                    a, b = get_indices(d["x"])
+                    row = row_to_write(currSplit[0], d["y"][a:b + 1])
+                    writer.writerow(row)
+
+            except:
+                print(f'error at {i}')
+
+            print(i)
+
+        cas.close()
